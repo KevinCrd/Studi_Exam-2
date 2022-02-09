2e0a72f20436bcd293fa1497f383bfa043f6de44
KevinCRD <kevin.cordonnier.projet@gmail.com>
2022-02-09 12:47:43 +0100

    1er depot
    - Creation des fichiers de base
    - Depot des images de Des
    - Depot d'une image du site demander decouper en plusieurs segments
    - Ecris dans Java.js de details techniques demander pour une meilleure organisation du code.

---
+++ java.js
@@ -0,0 +1,19 @@
+/* 
+
+New tour = remise de round a 0
+Si Des == 1, tour fini. 
+Round + Round + ect
+Hold = total des Round + global / Changement de joueur
+Si global == ou =< a 100 = joueur 1/2 a gagner
+
+-----------------
+
+Bouton new game = remise a zero des variables
+Bouton roll dice = lancer de des
+bouton hold = envoyer du resultat de round sur le global
+
+------------------
+
+2 variable round / 2 variable global / 1 variable lancer de des
+
+*/
