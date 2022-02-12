a7d988a486b4153cb7f1b2c5a036aec858873cd0
KevinCRD <kevin.cordonnier.projet@gmail.com>
2022-02-12 12:01:44 +0100

    Test lié a Java.js
    Ajout ligne pour le demarrage de java.js
    Test sur le fichier java d'un evement "click" et son fonctionnement

--- java.js
+++ java.js
@@ -17,3 +17,31 @@ bouton hold = envoyer du resultat de round sur le global
 2 variable round / 2 variable global / 1 variable lancer de des
 
 */
+
+let roundp1 = 0;
+let roundp2 = 0;
+let globalp1 = 0;
+let globalp2 = 0;
+let reboot = 0;
+
+
+/* Reset */
+document
+  .getElementById("ButtonNewGame")
+  .addEventListener("click", function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  
+document
+    .getElementById("score_p1_affichage")
+    .innerText = (globalp1 ) + '';
+document 
+    .getElementById("score_p2_affichage")
+    .innerText = (globalp2 ) + '';
+document
+    .getElementById("current_p1_affichage")
+    .innerText = (roundp1 ) + '';
+document
+    .getElementById("current_p2_affichage")
+    .innerText = (roundp2 ) + '';
+});
