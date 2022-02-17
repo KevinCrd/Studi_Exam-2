add72e78bb74a803e635ae81e76b52db510a2150
KevinCRD <kevin.cordonnier.projet@gmail.com>
2022-02-17 11:45:13 +0100

    Mise a jour Java.js
    - Mise en place d'un objet ( TableauScore ) Regrouper toutes les variables lié au score.
    - Fonction Reset pour remettre toutes les variables de tableauScore a 0
    - Ajout de commentaire coté Java
    - Fonction d'actualisation pour le score a chaque clic

--- java.js
+++ java.js
@@ -18,30 +18,53 @@ bouton hold = envoyer du resultat de round sur le global
 
 */
 
-let roundp1 = 0;
-let roundp2 = 0;
-let globalp1 = 0;
-let globalp2 = 0;
-let reboot = 0;
-
+let tableauScore = {
+roundp1: 54,
+roundp2: 0,
+globalp1: 0,
+globalp2: 10,
+};
 
 /* Reset */
-document
-  .getElementById("ButtonNewGame")
-  .addEventListener("click", function(e) {
-  e.preventDefault();
-  e.stopPropagation();
-  
-document
-    .getElementById("score_p1_affichage")
-    .innerText = (globalp1 ) + '';
-document 
-    .getElementById("score_p2_affichage")
-    .innerText = (globalp2 ) + '';
-document
-    .getElementById("current_p1_affichage")
-    .innerText = (roundp1 ) + '';
-document
-    .getElementById("current_p2_affichage")
-    .innerText = (roundp2 ) + '';
-});
+
+const ButtonNewGame = document.getElementById('ButtonNewGame');
+
+ButtonNewGame.addEventListener('click', Reset);
+
+function Reset() {
+
+    tableauScore.roundp1 = 0;
+    tableauScore.roundp2 = 0;
+    tableauScore.globalp1 = 0;
+    tableauScore.globalp2 = 0;
+    return tableauScore;
+
+}
+
+/* End Reset */ 
+
+/* Hold */
+/* End Hold */
+
+/* Roll Dice */
+/* End Roll Dice */
+
+
+/* Score */
+
+const DisplayScore = document.body;
+
+DisplayScore.addEventListener("click", FunctionScore);
+
+function FunctionScore() { 
+
+    document.getElementById("score_p1_affichage")
+    .innerText = (tableauScore.globalp1 ) + '';
+    document.getElementById("score_p2_affichage")
+    .innerText = (tableauScore.globalp2 ) + '';
+    document.getElementById("current_p1_affichage")
+    .innerText = (tableauScore.roundp1 ) + '';
+    document.getElementById("current_p2_affichage")
+    .innerText = (tableauScore.roundp2 ) + '';
+}
+/* End Score */
