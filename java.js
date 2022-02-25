0956081de7e730f8db498ce5b65dfdcbd1e984b5
KevinCRD <kevin.cordonnier.projet@gmail.com>
2022-02-25 12:17:15 +0100

    Correctif / Ajout lié au score
    - Correction " return "
    - Correction images, mauvais format
    - Correction des If / If else
    - Ajout du systeme de score

--- java.js
+++ java.js
@@ -20,10 +20,10 @@ bouton hold = envoyer du resultat de round sur le global
 
 let tableauScore = {
 player: 1,
-roundp1: 54,
+roundp1: 0,
 roundp2: 0,
 globalp1: 0,
-globalp2: 10,
+globalp2: 0,
 dicenumber: 0,
 };
 
@@ -34,25 +34,16 @@ ButtonHold.addEventListener('click', HoldChangePlayer);
 
 function HoldChangePlayer () {
 
-    if ( tableauScore.player == 1 ) 
+    if ( tableauScore.player == 1 ) {
         tableauScore.globalp1 += tableauScore.roundp1;
-
-    else if ( tableauScore.player == 1 && tableauScore.globalp1 > 100 )
-       Win /* rajouter ecran de win */
-
-    else
         tableauScore.roundp1 = 0;
-        tableauScore.player = 2; 
+    }
+    else if ( tableauScore.player == 2 ) {
+        tableauScore.globalp2 += tableauScore.roundp2; 
+        tableauScore.roundp2 = 0;
+    }
 
-    if ( tableauScore.player == 2 ) 
-        tableauScore.globalp2 += tableauScore.roundp2;
-
-    else if ( tableauScore.player == 2 && tableauScore.globalp1 > 100 )
-        Win /* rajouter ecran de win */
- 
-    else
-         tableauScore.roundp2 = 0;
-         tableauScore.player = 1; 
+    return tableauScore;
 
 }
 
@@ -71,6 +62,7 @@ function Reset() {
     tableauScore.globalp1 = 0;
     tableauScore.globalp2 = 0;
     tableauScore.dicenumber = 0;
+    tableauScore.player = 1;
     return tableauScore;
 
 }
@@ -80,13 +72,17 @@ function Reset() {
 /* Roll Dice */
 
 const ButtonRollDice = document.getElementById('ButtonRollDice');
-ButtonRollDice.addEventListener('click', Roll(1,6));
+let min = 1;
+let max = 6;
+ButtonRollDice.addEventListener('click', Roll);
 ButtonRollDice.addEventListener('click', ChangeDice);
+ButtonRollDice.addEventListener('click', Dice1);
+ButtonRollDice.addEventListener('click', Round);
 
-function Roll(min, max) {
+function Roll() {
 
     tableauScore.dicenumber = min + Math.floor(Math.random() * ( max - min +1));
-    return;
+    return tableauScore.dicenumber;
 }
 
     const ImageDice = document.getElementById("Dice");
@@ -94,36 +90,65 @@ function Roll(min, max) {
 
 function ChangeDice() {
 
-    if ( tableauScore.dicenumber == 1 ) 
-        DiceAttribute = "images/D1.png"
-        else if  ( tableauScore.dicenumber == 2 )
-            DiceAttribute = "images/D2.png"
-        else if  ( tableauScore.dicenumber == 3 )
-            DiceAttribute = "images/D3.png"
-        else if  ( tableauScore.dicenumber == 4 )
-            DiceAttribute = "images/D4.png"
-        else if  ( tableauScore.dicenumber == 5 )
-            DiceAttribute = "images/D5.png"
-        else
-            DiceAttribute = "images/D6.png"
+    if ( tableauScore.dicenumber == 1 ) {
+        DiceAttribute = "images/D1.jpg" 
+    }
+        else if  ( tableauScore.dicenumber == 2 ) {
+            DiceAttribute = "images/D2.jpg" 
+        }
+        else if  ( tableauScore.dicenumber == 3 ) {
+            DiceAttribute = "images/D3.jpg" 
+        }
+        else if  ( tableauScore.dicenumber == 4 ) {
+            DiceAttribute = "images/D4.jpg" 
+        }
+        else if  ( tableauScore.dicenumber == 5 ) {
+            DiceAttribute = "images/D5.jpg" 
+        }
+        else if ( tableauScore.dicenumber == 6 ) {
+            DiceAttribute = "images/D6.jpg" 
+        }
 
-    if ( tableauScore.dicenumber == 1 && tableauScore.player == 1 )
+         return DiceAttribute;
+
+}
+function Dice1() {
+
+    if ( tableauScore.dicenumber == 1 && tableauScore.player == 1 ){
         tableauScore.roundp1 = 0;
+        tableauScore.dicenumber = 0;
         tableauScore.player = 2;
+        
+    }
+        else if ( tableauScore.dicenumber == 1 && tableauScore.player == 2 ){
+    
+            tableauScore.roundp2 = 0;
+            tableauScore.dicenumber = 0;
+            tableauScore.player = 1; 
+            
+        }
 
-    if ( tableauScore.dicenumber == 1 && tableauScore.player == 2 )
-        tableauScore.roundp2 = 0;
-        tableauScore.player = 1;
-
-    return tableauScore;
-
-}    
+}
+   
     
 /* End Roll Dice */
 
 /* Round */ 
 
+function Round() {
 
+    if ( tableauScore.player == 1 ) {
+    tableauScore.roundp1 += tableauScore.dicenumber 
+    tableauScore.dicenumber = 0;
+    }
+        else if ( tableauScore.player == 2 ) {
+        tableauScore.roundp2 += tableauScore.dicenumber;
+        tableauScore.dicenumber = 0;
+        }
+
+        return tableauScore;
+
+}
 
 /* End Round */
 
@@ -138,7 +163,7 @@ function FunctionScore() {
     document.getElementById("score_p1_affichage")
     .innerText = (tableauScore.globalp1) + '';
     document.getElementById("score_p2_affichage")
-    .innerText = (tableauScore.dicenumber) + '';
+    .innerText = (tableauScore.globalp2) + '';
     document.getElementById("current_p1_affichage")
     .innerText = (tableauScore.roundp1) + '';
     document.getElementById("current_p2_affichage")
